a6745f3a92969f8a24ff448ccf6a4a06ff858d6f
bobo <785817955@qq.com>
2022-10-31 00:07:01 +0800

    feat(cmp): 代码自动补全

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -61,6 +61,18 @@ packer.startup({
     -- lsp
     use("williamboman/nvim-lsp-installer")
     use({ "neovim/nvim-lspconfig" })
+    -- 补全引擎
+    use("hrsh7th/nvim-cmp")
+    -- snippet 引擎
+    use("hrsh7th/vim-vsnip")
+    -- 补全源
+    use("hrsh7th/cmp-vsnip")
+    use("hrsh7th/cmp-nvim-lsp") -- { name = nvim_lsp }
+    use("hrsh7th/cmp-buffer") -- { name = 'buffer' },
+    use("hrsh7th/cmp-path") -- { name = 'path' }
+    use("hrsh7th/cmp-cmdline") -- { name = 'cmdline' }
+    -- 常见编程语言代码段
+    use("rafamadriz/friendly-snippets")
 
     if paccker_bootstrap then
       packer.sync()
